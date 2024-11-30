eed40b7b62cff52895102d04788bad93df8923c0
Master Oogway <125769645+ActualMasterOogway@users.noreply.github.com>
2024-11-30 11:29:11 +0100

    Delete src directory

--- src/Loader.lua
+++
@@ -1,7 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Astral-Softworks/Astral-Public/main/src/Games.lua"))()
-
-local Game = Games[game.GameId]
-
-if Game then
-    loadstring(game:HttpGet(`https://raw.githubusercontent.com/Astral-Softworks/Astral-Public/main/src/{Game.Name}.lua`))()
-end
